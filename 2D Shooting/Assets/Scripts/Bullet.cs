da007125a1f66f3a9bf04e5bf437778712674a41
AJSands <44375377+AJSands@users.noreply.github.com>
2018-11-14 18:00:25 -0500

    Added testbed for prefab-shooting weapons ("MachineGun").
    Added autofire, fire rate, bullet spread, pellet count, and some collision layers.

--- 2D Shooting/Assets/Scripts/Bullet.cs
+++ 2D Shooting/Assets/Scripts/Bullet.cs
@@ -2,29 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
 
-	public float speed = 20f;
-	public int damage = 40;
-	public Rigidbody2D rb;
-	public GameObject impactEffect;
+    public int damage = 40;
+    public float speed = 20f;
+    public float lifetime = 1f; // Time in seconds before bullet is automatically destroyed.
+    //public bool explodeOnMiss = false;
+    //public bool piercesEntities = false;
+    //public bool piercesWalls = false;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
 
-	// Use this for initialization
-	void Start () {
-		rb.velocity = transform.right * speed;
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
 
-	void OnTriggerEnter2D (Collider2D hitInfo)
-	{
-		Enemy enemy = hitInfo.GetComponent<Enemy>();
-		if (enemy != null)
-		{
-			enemy.TakeDamage(damage);
-		}
+    void Awake()
+    {
+        //TODO: Object pooling.
+        Destroy(gameObject, lifetime);
 
-		Instantiate(impactEffect, transform.position, transform.rotation);
+        //TODO: Explode on miss.
+    }
 
-		Destroy(gameObject);
-	}
-	
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        //TODO: System for choosing collision targets depending on whether bullet was fired by player or enemy.
+        //TODO: Piercing.
+        if (hitInfo.gameObject.layer != 8 &&
+            hitInfo.gameObject.layer != 10 &&
+            hitInfo.gameObject.layer != 11)
+        {
+            Enemy enemy = hitInfo.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            //TODO: Object pooling.
+            Instantiate(impactEffect, transform.position, transform.rotation);
+
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ 2D Shooting/Assets/Scripts/MachineGun.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineGun : MonoBehaviour
+{
+    public Transform firePoint; // The origin point for this weapon's projectiles (i.e. the gun nozzle).
+    public GameObject bulletPrefab; // The type of projectile launched from this weapon.
+
+    public bool auto = true; // True: Weapon shoots as long as trigger is held. False: Weapon shoots once per trigger pull.
+    public float fireRate = 0.075f; // Delay between shots, in seconds.
+    //public int magazineCapacity = 50;
+    //public float reloadTime = 2f;
+    //public int ammo = 500;
+    public int pelletCount = 1; // Number of projectiles fired at once.
+    public float sprayRange = 4f; // Size of arc bullets may randomly fire in, in degrees.
+
+    private float lastShotTime; // Time last shot was fired, for calculating delay before next shot.
+
+    void Update()
+    {
+        if (auto)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        //TODO: Charge beam? Might need to go in its own class.
+    }
+
+    void Shoot()
+    {
+        if (Time.time > lastShotTime + fireRate)
+        {
+            for (int i = 0; i < pelletCount; i++)
+            {
+                float spread = Random.Range(-sprayRange / 2, sprayRange / 2);
+                Quaternion bulletRotation = firePoint.rotation * Quaternion.Euler(0, 0, spread);
+
+                //TODO: Object pooling.
+                Instantiate(bulletPrefab, firePoint.position, bulletRotation);
+                lastShotTime = Time.time;
+            }
+        }
+    }
+}
